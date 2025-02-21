eff7b705087affa0feead2d5429c134b38338488
Milan Agarwal <milan@Milan_Agarwal.com>
2025-02-21 22:50:28 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const app = express();
+const db = require('./db');
+require('dotenv').config();
+
+const bodyParser = require('body-parser'); 
+app.use(bodyParser.json()); // req.body
+const cors = require('cors');
+app.use(cors({ origin: 'http://myvoting-app1.netlify.app' }));
+const PORT = process.env.PORT || 3000;
+
+// Import the router files
+const userRoutes = require('./routes/userRoutes');
+const candidateRoutes = require('./routes/candidateRoutes');
+
+// Use the routers
+app.use('/user', userRoutes);
+app.use('/candidate', candidateRoutes);
+
+
+app.listen(PORT, ()=>{
+    console.log('listening on port 3000');
+})
